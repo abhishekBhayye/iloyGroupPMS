ba19ffeed634566bb8d9304d24e2eaa89d1b7b73
bhayyeabhishek@gmail.com <bhayyeabhishek@gmail.com>
2020-11-24 00:34:53 +0000

    Adding Angular routing and new manage component.

--- iloyPeopleManagementSystem/src/app/app.module.ts
+++ iloyPeopleManagementSystem/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
